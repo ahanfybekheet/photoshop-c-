31ecab8e2973e6f1808614055c9072de26fc056c
Abdelrhman Mostafa <abdelrhman.m.hessain@gmail.com>
2022-04-02 19:45:08 +0200

    flip vertical and horizontal
    edit flip function to flip vertical and horizontal

--- PHOTOSHOP.cpp
+++ PHOTOSHOP.cpp
@@ -15,6 +15,7 @@
 using namespace std;
 unsigned char image[SIZE][SIZE];
 unsigned char image2[SIZE][SIZE];
+unsigned char image3[127][127];
 char userInput;
 
 
@@ -25,8 +26,9 @@ void BW();
 void invertFilter();
 void mergeImage();
 void flipImage();
-void rotate();
 void darkenAndLightenImage();
+void rotate();
+void enlarge();
 
 
 int main()
@@ -51,13 +53,15 @@ int main()
     case 52:
         flipImage();
         break;
+    case 53:
+        darkenAndLightenImage();
+        break;
     case 54:
         rotate();
         break;
-
-    case 53:
-        darkenAndLightenImage();
-
+    case 56:
+        enlarge();
+        break;
     default:
         break;
     }
@@ -118,13 +122,13 @@ void saveImage() {
 
     // Add to it .bmp extension and load image
     strcat(imageFileName, ".bmp");
-    if (userInput == '4' || userInput == '6' )
+    if (userInput == '4' || userInput == '6' || userInput == '8' ){
         writeGSBMP(imageFileName, image2);
+    }
     else {
         writeGSBMP(imageFileName, image);
     }
 }
-
 //_________________________________________
 void BW() {
     for (int i = 0; i < SIZE; i++) {
@@ -139,7 +143,6 @@ void BW() {
         }
     }
 }
-
 //_________________________________________
 void invertFilter(){
     for (int i = 0; i < SIZE; i++) {
@@ -170,14 +173,35 @@ void mergeImage(){
 }
 //_________________________________________
 void flipImage(){
-    for (int i =0 ;i <255 ;i++){
-        for (int j =0 ;j <255 ;j++){
-            image2[i][j] = image [225-i][j];
+    int choose;
+    while (true)
+    {
+        cout << "Flip (1) for horizontally or (2) for vertically: ";
+        cin >> choose;
+        if (choose == 1 || choose == 2){
+            break;
         }
-    } 
+        else {
+            cout << "Invalid input.";
+            cin.ignore();
+        }
+    }
+    if (choose == 1){
+        for (int i =0 ;i <255 ;i++){
+            for (int j =255 ;j > 0 ;j--){
+                image2[i][255 - j] = image [i][j];
+                
+            }
+        } 
+    }
+    else {
+        for (int i =0 ;i <255 ;i++){
+            for (int j =0 ;j <255 ;j++){
+                image2[i][j] = image [255-i][j];
+            }
+        } 
+    }
 }
-
-
 //_________________________________________
 void darkenAndLightenImage(){
     int chooses;
@@ -217,7 +241,6 @@ void darkenAndLightenImage(){
     }
 
 }
-
 //_________________________________________
 void rotate(){
     int rot;
@@ -228,7 +251,7 @@ void rotate(){
         if (rot == 90 || rot == 180 || rot == 270)
             break;
         else{
-            cout<<"Please choose valid input\n";
+            cout<<"Please enter valid number\n";
             cin.ignore();
         }
     }
@@ -263,4 +286,50 @@ void rotate(){
             }
         }
     }
-} 
+}
+//_________________________________________
+void enlarge(){
+    int quarter;
+    while (true)
+    {
+        cout << "Please enter the Quarter number (1, 2, 3, 4) you want to enlarge: ";
+        cin >> quarter;
+        if (quarter == 1 || quarter == 2 || quarter == 3 || quarter == 4){
+            break;
+        }
+        else{
+            cout << "Please enter valid number\n";
+            cin.ignore();
+        }
+    }
+    if (quarter == 1){
+        for (int i = 0; i < 127; i ++){
+            for (int j = 0; j < 127; j ++){
+                image2[i][j] = image[i][j];
+                
+                
+            }
+        }
+    }
+    if (quarter == 2){
+        for (int i = 0; i < 127; i ++){
+            for (int j = 127; j < 255; j ++){
+                image2[i][j] = image[i][j];
+            }
+        }
+    }
+    if (quarter == 3){
+        for (int i = 127; i < 255; i ++){
+            for (int j = 0; j < 127; j ++){
+                image2[i][j] = image[i][j];
+            }
+        }
+    }
+    if (quarter == 4){
+        for (int i = 127; i < 255; i ++){
+            for (int j = 127; j < 255; j ++){
+                image2[i][j] = image[i][j];
+            }
+        }
+    }
+}
